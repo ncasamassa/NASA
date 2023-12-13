ddbafb7e0730b342578dfe366ccbca8d0d9e994f
Nick Casamassa <nickabod.crane@gmail.com>
2023-12-12 18:54:37 -0800

    finished

--- script.js
+++ script.js
@@ -1,69 +1,44 @@
 let currentDate = new Date();
-let yyyy = currentDate.getFullYear();
-let mm = String(currentDate.getMonth() + 1).padStart(2, '0');
-let dd = String(currentDate.getDate()).padStart(2, '0');
-let formattedCurrentDate = `${yyyy}-${mm}-${dd}`;
 let index = 0;
-const prevButton = document.getElementById('prevButton');
-const nextButton = document.getElementById('nextButton');
+const prevButton = document.getElementById("prevButton");
+const nextButton = document.getElementById("nextButton");
 
-document.addEventListener("DOMContentLoaded", function () {
-    fetchAPOD(formattedCurrentDate);
-});
+const fetchAPOD = (idx) => {
+  currentDate.setDate(currentDate.getDate() - idx);
+  let yyyy = currentDate.getFullYear();
+  let mm = String(currentDate.getMonth() + 1).padStart(2, "0");
+  let dd = String(currentDate.getDate()).padStart(2, "0");
+  let formattedCurrentDate = `${yyyy}-${mm}-${dd}`;
+  console.log(formattedCurrentDate);
 
-const fetchAPOD = (date) => {
-    const apiKey = 'bfvtqtEz8qnHxObZDIzLNxHPPqPtaPB33yRcajPM';
-    const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${date}`;
+  const apiKey = "bfvtqtEz8qnHxObZDIzLNxHPPqPtaPB33yRcajPM";
+  const API_URL = `https://api.nasa.gov/planetary/apod?api_key=${apiKey}&date=${formattedCurrentDate}`;
 
-    fetch(API_URL)
-        .then(response => response.json())
-        .then(data => {
-            displayAPOD(data.url, data.title, data.explanation);
-        })
-        .catch(error => console.error('Error fetching APOD:', error));
+  fetch(API_URL)
+    .then((response) => response.json())
+    .then((data) => {
+      displayAPOD(data.url, data.title, data.explanation);
+    })
+    .catch((error) => console.error("Error fetching APOD:", error));
 };
 
-    const displayAPOD = (imageUrl, title, explanation) => {
-        const imageElement = document.createElement('img');
-        imageElement.src = imageUrl;
+const displayAPOD = (imageUrl, title, explanation) => {
+  const imageElement = document.createElement("img");
+  imageElement.src = imageUrl;
 
-        const titleElement = document.createElement('h2');
-        titleElement.textContent = title;
+  const titleElement = document.createElement("h2");
+  titleElement.textContent = title;
 
-        const explanationElement = document.createElement('p');
-        explanationElement.textContent = explanation;
+  const explanationElement = document.createElement("p");
+  explanationElement.textContent = explanation;
 
-        const mainPictureContainer = document.getElementById('main-picture');
-        mainPictureContainer.innerHTML = '';
-        mainPictureContainer.appendChild(imageElement);
-        mainPictureContainer.appendChild(titleElement);
-        mainPictureContainer.appendChild(explanationElement);
+  const mainPictureContainer = document.getElementById("main-picture");
+  mainPictureContainer.innerHTML = "";
+  mainPictureContainer.appendChild(imageElement);
+  mainPictureContainer.appendChild(titleElement);
+  mainPictureContainer.appendChild(explanationElement);
+};
 
-        index++;
-        // updateNavigation();
-    };
-
-// const updateNavigation = () => {
-
-        let yesterday = new Date();
-        yesterday.setDate(yesterday.getDate() - index);
-        
-        let yyyy2 = yesterday.getFullYear();
-        let mm2 = String(yesterday.getMonth() + 1).padStart(2, '0');
-        let dd2 = String(yesterday.getDate()).padStart(2, '0');
-        
-        let formattedYesterday = `${yyyy2}-${mm2}-${dd2}`;
-        console.log(formattedYesterday)
-
-        // let tomorrow = yesterday.setDate(yesterday.getDate() - (index - 1));
-        
-        // let yyyy2 = tomorrow.getFullYear();
-        // let mm2 = String(tomorrow.getMonth() + 1).padStart(2, '0');
-        // let dd2 = String(tomorrow.getDate()).padStart(2, '0');
-        
-        // let formattedTomorrow = `${yyyy2}-${mm2}-${dd2}`;
-
-        prevButton.addEventListener('click', () => fetchAPOD(formattedYesterday));
-        // nextButton.addEventListener('click', () => fetchAPOD(formattedTomorrow));
-
-    // };
+fetchAPOD(index);
+prevButton.addEventListener("click", () => fetchAPOD(index + 1));
+nextButton.addEventListener("click", () => fetchAPOD(index - 1));
